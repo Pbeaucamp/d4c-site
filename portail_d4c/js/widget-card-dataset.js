f46eed399b1ff40926a467e91e006b07d94c9312
Kevin Monnery <kevin.monnery@bpm-conseil.com>
2020-07-24 12:23:43 +0000

    features/widget-card-dataset

--- portail_d4c/js/widget-card-dataset.js
+++ portail_d4c/js/widget-card-dataset.js
@@ -1,12 +1,19 @@
-function calculValueFromFiltre(elem, idRes, colonne, colonne_filtre, valeur_filtre, operation) {
+function calculValueFromFiltre(elem, idRes, colonne, operation,colonne_filtre=null, valeur_filtre =null ) {
+
 	$.ajax('/api/calculvaluefiltre', {
         	type: 'POST',
                 dataType: "json",
                 cache: true,
                 data : { idRes : idRes, colonne : colonne, colonne_filtre: colonne_filtre,  valeur_filtre: valeur_filtre, operation : operation },
                 success: function (data) {
-                	$( "#"+elem ).append( "<p>"+data.result+"</p>" );
+                        var resultat = numberWithCommas(parseInt(data.result));
+                	$( "#"+elem ).append( "<p>"+resultat+"</p>" );
+                     
                 	
                 }
 	});
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 }
